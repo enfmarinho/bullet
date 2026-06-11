cc4e184b6066cea1a06369b86f6b6e7d2003ab02
enfmarinho <eduardomarinho@pm.me>
2026-06-11 11:23:33 -0300

    m22 v1

--- examples/minke.rs
+++ examples/minke.rs
@@ -16,7 +16,7 @@ use bullet_lib::{
 };
 use std::{fs, path::Path};
 
-const OUTPUT_DIRECTORY: &str = "checkpoints/minke21/v1";
+const OUTPUT_DIRECTORY: &str = "checkpoints/minke22/v1";
 const BINPACK_PATH: &str = "data/selfgen/interleaved_7-19.vf";
 const N_THREADS: usize = 4;
 const BUFFER_SIZE_MB: usize = 2048;
@@ -33,7 +33,7 @@ const INITIAL_LR: f32 = 1e-3;
 const FINAL_LR: f32 = 1e-6;
 
 const INITIAL_WDL: f32 = 0.20;
-const FINAL_WDL: f32 = 0.60;
+const FINAL_WDL: f32 = 0.40;
 
 const HIDDEN_SIZE: usize = 1024;
 const SCALE: f32 = 400.0;
