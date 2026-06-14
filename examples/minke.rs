114bc3cbb719378d41ce0dcdb2bb57110f522a52
enfmarinho <eduardomarinho@pm.me>
2026-06-14 14:38:05 -0300

    m24 v3

--- examples/minke.rs
+++ examples/minke.rs
@@ -16,8 +16,8 @@ use bullet_lib::{
 };
 use std::{fs, path::Path};
 
-const OUTPUT_DIRECTORY: &str = "checkpoints/minke23/v1";
-const BINPACK_PATH: &str = "data/selfgen/interleaved_7-22.vf";
+const OUTPUT_DIRECTORY: &str = "checkpoints/minke24/v3";
+const BINPACK_PATH: &str = "data/selfgen/interleaved_7-23.vf";
 const N_THREADS: usize = 4;
 const BUFFER_SIZE_MB: usize = 2048;
 
