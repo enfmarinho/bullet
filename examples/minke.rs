9cf57e78f9aa77fb87ea33d529a9c3e2f62bd827
enfmarinho <eduardomarinho@pm.me>
2026-08-03 07:49:59 -0300

    m35 v1

--- examples/minke.rs
+++ examples/minke.rs
@@ -17,8 +17,8 @@ use bullet_lib::{
 use std::{fs, path::Path};
 
 const CHECKPOINT_PATH: &str = "";
-const OUTDIR: &str = "checkpoints/minke34/v3";
-const DATASET_PATH: &str = "data/selfgen/interleaved_12-28.vf";
+const OUTDIR: &str = "checkpoints/minke35/v1";
+const DATASET_PATH: &str = "data/selfgen/interleaved_12-33.vf";
 const N_THREADS: usize = 4;
 const BUFFER_SIZE_MB: usize = 2048;
 
