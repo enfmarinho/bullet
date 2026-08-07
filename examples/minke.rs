44f0655d8d583cadac2ea8adf8595f97f4f79430
enfmarinho <eduardomarinho@pm.me>
2026-08-06 22:15:04 -0300

    m37 v3

--- examples/minke.rs
+++ examples/minke.rs
@@ -17,7 +17,7 @@ use bullet_lib::{
 use std::{fs, path::Path};
 
 const CHECKPOINT_PATH: &str = "";
-const OUTDIR: &str = "checkpoints/minke37/v2";
+const OUTDIR: &str = "checkpoints/minke37/v3";
 const DATASET_PATH: &str = "data/selfgen/interleaved_12-33.vf";
 const N_THREADS: usize = 4;
 const BUFFER_SIZE_MB: usize = 2048;
@@ -33,10 +33,11 @@ const BATCHES_PER_SUPERBATCH: usize = 6104;
 const SAVE_RATE: usize = 25;
 const INITIAL_LR: f32 = 1e-3;
 const FINAL_LR: f32 = 1e-6;
-const FINETUNE_LR: f32 = 1e-6;
+const FINETUNE_INITIAL_LR: f32 = 1e-5;
+const FINETUNE_FINAL_LR: f32 = 1e-7;
 
 const INITIAL_WDL: f32 = 0.20;
-const FINAL_WDL: f32 = 0.60;
+const FINAL_WDL: f32 = 0.40;
 const FINETUNE_WDL: f32 = 0.90;
 
 const SCALE: f32 = 400.0;
@@ -215,7 +216,11 @@ fn main() {
             end_superbatch: END_FINETUNE_SB,
         },
         wdl_scheduler: wdl::ConstantWDL { value: FINETUNE_WDL },
-        lr_scheduler: lr::ConstantLR { value: FINETUNE_LR },
+        lr_scheduler: lr::LinearDecayLR {
+            initial_lr: FINETUNE_INITIAL_LR,
+            final_lr: FINETUNE_FINAL_LR,
+            final_superbatch: END_FINETUNE_SB,
+        },
         save_rate: SAVE_RATE,
     };
     trainer.run(&finetune_schedule, &settings, &data_loader);
