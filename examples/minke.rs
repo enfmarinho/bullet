6cabf26dfefe70998114143da38cec110b18d73e
enfmarinho <eduardomarinho@pm.me>
2026-08-16 21:28:55 -0300

    m38 v2

--- examples/minke.rs
+++ examples/minke.rs
@@ -12,12 +12,21 @@ use bullet_lib::{
         schedule::{lr, wdl, TrainingSchedule, TrainingSteps},
         settings::LocalSettings,
     },
-    value::{loader, ValueTrainerBuilder},
+    value::{
+        loader::{self, ViriFilter},
+        ValueTrainerBuilder,
+    },
 };
+use rand::{rng, Rng};
+use std::cell::{Cell, RefCell};
 use std::{fs, path::Path};
+use viriformat::{
+    chess::{board::Board, chessmove::Move},
+    dataformat::WDL,
+};
 
 const CHECKPOINT_PATH: &str = "";
-const OUTDIR: &str = "checkpoints/minke38/v1";
+const OUTDIR: &str = "checkpoints/minke38/v2";
 const DATASET_PATH: &str = "data/selfgen/interleaved_12-33.vf";
 const N_THREADS: usize = 4;
 const BUFFER_SIZE_MB: usize = 2048;
@@ -27,8 +36,8 @@ const END_FIRST_SB: usize = 100;
 const END_SECOND_SB: usize = 600;
 const END_FINETUNE_SB: usize = 800;
 
-const BATCH_SIZE: usize = 16_384 * 8;
-const BATCHES_PER_SUPERBATCH: usize = 6104 / 8;
+const BATCH_SIZE: usize = 16_384;
+const BATCHES_PER_SUPERBATCH: usize = 6104;
 
 const SAVE_RATE: usize = 25;
 const INITIAL_LR: f32 = 1e-3;
@@ -76,6 +85,76 @@ fn save_config() {
     let dest_path = Path::new(OUTDIR).join("config.rs");
     let _ = fs::create_dir_all(OUTDIR);
     let _ = fs::write(dest_path, SOURCE_CODE);
+}
+
+fn piece_count_acceptance(board: &Board) -> f64 {
+    // from pawnnochio training scripts
+    #[rustfmt::skip]
+    const DESIRED_DISTRIBUTION: [f64; 33] = [
+        0.018411966423, 0.020641545085, 0.022727271053,
+        0.024669162740, 0.026467201733, 0.028121406444,
+        0.029631758462, 0.030998276198, 0.032220941240,
+        0.033299772000, 0.034234750067, 0.035025893853,
+        0.035673184944, 0.036176641754, 0.036536245870,
+        0.036752015705, 0.036823932846, 0.036752015705,
+        0.036536245870, 0.036176641754, 0.035673184944,
+        0.035025893853, 0.034234750067, 0.033299772000,
+        0.032220941240, 0.030998276198, 0.029631758462,
+        0.028121406444, 0.026467201733, 0.024669162740,
+        0.022727271053, 0.020641545085, 0.018411966423,
+    ];
+
+    thread_local! {
+        static PIECE_COUNT_STATS: RefCell<[u64; 33]> = const { RefCell::new([0; 33]) };
+        static PIECE_COUNT_TOTAL: Cell<u64> = const { Cell::new(0) };
+    }
+
+    let pc = board.pieces.occupied().count() as usize;
+    let count = PIECE_COUNT_STATS.with_borrow_mut(|stats| {
+        stats[pc] += 1;
+        stats[pc]
+    });
+    let total = PIECE_COUNT_TOTAL.with(|t| {
+        let total = t.get() + 1;
+        t.set(total);
+        total
+    });
+    let frequency = count as f64 / total as f64;
+
+    let acceptance = 0.5 * DESIRED_DISTRIBUTION[pc] / frequency;
+    acceptance.clamp(0., 1.)
+}
+
+fn filter(board: &Board, mv: Move, eval: i16, wdl: f32) -> bool {
+    let viri_filter = loader::viribinpack::Filter {
+        min_ply: 16,
+        min_pieces: 4,
+        max_eval: 10000,
+        filter_tactical: true,
+        filter_check: true,
+        filter_castling: false,
+        max_eval_incorrectness: u32::MAX,
+        random_fen_skipping: true,
+        random_fen_skip_probability: 0.50,
+
+        wdl_filtered: false,
+        wdl_model_params_a: [0.0; 4],
+        wdl_model_params_b: [0.0; 4],
+
+        material_min: 17,
+        material_max: 78,
+        mom_target: 58,
+        wdl_heuristic_scale: 1.5,
+    };
+    let wdl = match wdl {
+        1.0 => WDL::Win,
+        0.5 => WDL::Draw,
+        0.0 => WDL::Loss,
+        _ => unreachable!(),
+    };
+    let mut rng = rng();
+
+    !viri_filter.should_filter(mv, eval as i32, board, wdl, &mut rng) && rng.random_bool(piece_count_acceptance(board))
 }
 
 fn main() {
@@ -158,30 +237,8 @@ fn main() {
 
     let settings = LocalSettings { threads: N_THREADS, test_set: None, output_directory: OUTDIR, batch_queue_size: 64 };
 
-    let data_loader = {
-        let filter = loader::viribinpack::Filter {
-            min_ply: 16,
-            min_pieces: 4,
-            max_eval: 10000,
-            filter_tactical: true,
-            filter_check: true,
-            filter_castling: false,
-            max_eval_incorrectness: u32::MAX,
-            random_fen_skipping: true,
-            random_fen_skip_probability: 0.50,
-
-            wdl_filtered: false,
-            wdl_model_params_a: [0.0; 4],
-            wdl_model_params_b: [0.0; 4],
-
-            material_min: 17,
-            material_max: 78,
-            mom_target: 58,
-            wdl_heuristic_scale: 1.5,
-        };
-
-        loader::ViriBinpackLoader::new(DATASET_PATH, BUFFER_SIZE_MB, N_THREADS, filter)
-    };
+    let data_loader =
+        loader::ViriBinpackLoader::new(DATASET_PATH, BUFFER_SIZE_MB, N_THREADS, ViriFilter::Custom(filter));
 
     if !CHECKPOINT_PATH.is_empty() {
         trainer.load_from_checkpoint(CHECKPOINT_PATH);
