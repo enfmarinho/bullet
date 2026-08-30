362f2edd8af2de96ba96cdacebd006b89bbf6074
enfmarinho <eduardonestormarinho228@gmail.com>
2026-08-30 19:49:17 -0300

    Move config default values out of main src file

--- examples/minke/main.rs
+++ examples/minke/main.rs
@@ -1,8 +1,7 @@
+mod config;
+
 use bullet_lib::{
-    game::{
-        inputs::{get_num_buckets, ChessBucketsMirrored},
-        outputs::MaterialCount,
-    },
+    game::{inputs::ChessBucketsMirrored, outputs::MaterialCount},
     nn::{
         optimiser::{AdamW, AdamWParams},
         InitSettings, Shape,
@@ -17,75 +16,13 @@ use bullet_lib::{
         ValueTrainerBuilder,
     },
 };
+use config::*;
 use rand::{rng, Rng};
 use std::cell::{Cell, RefCell};
-use std::{fs, path::Path};
 use viriformat::{
     chess::{board::Board, chessmove::Move},
     dataformat::WDL,
 };
-
-const CHECKPOINT_PATH: &str = "";
-const OUTDIR: &str = "checkpoints/minke40/v2";
-const DATASET_PATH: &str = "data/selfgen/interleaved_12-33.vf";
-const N_THREADS: usize = 4;
-const BUFFER_SIZE_MB: usize = 2048 * 4;
-
-const START_SB: usize = 1;
-const END_FIRST_SB: usize = 100;
-const END_SECOND_SB: usize = 600;
-const END_FINETUNE_SB: usize = 800;
-
-const BATCH_SIZE: usize = 16_384;
-const BATCHES_PER_SUPERBATCH: usize = 6104;
-
-const SAVE_RATE: usize = 25;
-const INITIAL_LR: f32 = 1e-3;
-const FINAL_LR: f32 = 1e-6;
-const FINETUNE_INITIAL_LR: f32 = 1e-5;
-const FINETUNE_FINAL_LR: f32 = 1e-7;
-
-const INITIAL_WDL: f32 = 0.20;
-const FINAL_WDL: f32 = 0.40;
-const FINETUNE_WDL: f32 = 1.00;
-
-const SCALE: f32 = 400.0;
-
-// Quantization
-const QA: i16 = 255;
-const QB: i16 = 128;
-const QC: i32 = 64;
-
-const L1_SHIFT: usize = 8;
-const L1_SHIFT_SCALE: f32 = QA as f32 / (1 << L1_SHIFT) as f32;
-const I8_RANGE: f32 = i8::MAX as f32 / QB as f32;
-const L1_RANGE: f32 = I8_RANGE * L1_SHIFT_SCALE * L1_SHIFT_SCALE;
-
-// arch
-const L1_SIZE: usize = 1280;
-const L2_SIZE: usize = 16;
-const L3_SIZE: usize = 32;
-
-#[rustfmt::skip]
-const BUCKET_LAYOUT: [usize; 32] = [
-    0,  1,  2,  3,
-    4,  5,  6,  7,
-    8,  8,  9,  9,
-    10, 10, 11, 11,
-    12, 12, 13, 13,
-    12, 12, 13, 13,
-    14, 14, 15, 15,
-    14, 14, 15, 15,
-];
-const NUM_INPUT_BUCKETS: usize = get_num_buckets(&BUCKET_LAYOUT);
-const NUM_OUTPUT_BUCKETS: usize = 8;
-
-static SOURCE_CODE: &str = include_str!("minke.rs");
-fn save_config() {
-    let dest_path = Path::new(OUTDIR).join("config.rs");
-    let _ = fs::create_dir_all(OUTDIR);
-    let _ = fs::write(dest_path, SOURCE_CODE);
-}
 
 fn piece_count_acceptance(board: &Board) -> f64 {
     // from pawnnochio training scripts
